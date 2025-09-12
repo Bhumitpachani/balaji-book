2ebb40f83c326b731aa68e79d2c0aec2f93a64fe
Bhumitpachani <131967410+Bhumitpachani@users.noreply.github.com>
2025-09-12 11:55:10 +0530

    Update PWAInstallPrompt.tsx

--- src/components/common/PWAInstallPrompt.tsx
+++ src/components/common/PWAInstallPrompt.tsx
@@ -14,7 +14,7 @@ export const PWAInstallPrompt: React.FC = () => {
       <Card className="p-4 bg-card border shadow-lg">
         <div className="flex items-center gap-3">
             <img 
-            src="/icons/icon-152x152.png" 
+            src="https://5.imimg.com/data5/YD/VE/MY-27589869/balaji-engineering-works-90x90.jpg" 
             alt="BalajiBook App Icon" 
             className="w-12 h-12 rounded-lg flex-shrink-0 object-cover"
           />
