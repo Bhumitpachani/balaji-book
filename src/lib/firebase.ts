f44a60ebb890ebf9f7117bd8a4655f4b44d39fdb
Bhumitpachani <131967410+Bhumitpachani@users.noreply.github.com>
2025-11-16 19:49:05 +0530

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,13 +3,13 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  // apiKey: "AIzaSyBSfHugjPJ52QBQSuHWt9iGBFhq6wBoh5g",
-  // authDomain: "hotel-7ac9f.firebaseapp.com",
-  // projectId: "hotel-7ac9f",
-  // storageBucket: "hotel-7ac9f.appspot.com",
-  // messagingSenderId: "102297833385",
-  // appId: "1:102297833385:web:f5605f4331c2b5f7173eb8",
-  // measurementId: "G-1TZYLRZXES"
+apiKey: "AIzaSyBSfHugjPJ52QBQSuHWt9iGBFhq6wBoh5g",
+  authDomain: "hotel-7ac9f.firebaseapp.com",
+  projectId: "hotel-7ac9f",
+  storageBucket: "hotel-7ac9f.appspot.com",
+  messagingSenderId: "102297833385",
+  appId: "1:102297833385:web:f5605f4331c2b5f7173eb8",
+  measurementId: "G-1TZYLRZXES"
 };
 
 export const app = initializeApp(firebaseConfig);
